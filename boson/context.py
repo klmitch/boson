6dcf8bf27057b5e04f42cf8b5298dddeaa178b84
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-09-06 11:50:41 -0500

    Rough in a context.py and pull in more from openstack-common

---
+++ boson/context.py
@@ -0,0 +1,40 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2012 OpenStack LLC.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from boson.db import api as db_api
+from boson.openstack.common import log as logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+class RequestContext(common_context.RequestContext):
+    def __init__(self, user, tenant, roles=None, **kwargs):
+        if kwargs:
+            LOG.warn(_('Arguments dropped when creating context: %s') %
+                     str(kwargs))
+
+        self.user = user
+        self.tenant = tenant
+        self.roles = roles or []
+        self._session = None
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = db_api.get_session()
+        return self._session
